cbf7f9261270c8c599f7fed58eec40cf9855bd64
William Nascimento <william.nascimento2010@gmail.com>
2026-02-05 16:17:49 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   // URL_API: 'http://localhost:80÷80',
   // URL_API: 'http://95.111.226.231:32060',
   // URL_API: 'https://backend.sjc-laser-agenda.site',
-    URL_API: 'https://backend.agenda-an-locacoes.site',
+    URL_API: 'https://backend.belacrio-agenda.online',
 
   hmr: false,  
   role: ''
